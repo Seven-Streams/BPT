ce627cfe7d20e1c2801eb389aa8c9005f088ea37
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-03-12 09:57:36 +0800

    feat:import the memoryriver.

---
+++ BPT.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <filesystem>
+const unsigned long long mode1 = 13331, mode2 = 131;
+template <class W, int info_len = 2> class MemoryRiver {
+private:
+  std::fstream file;
+  std::string file_name;        // 文件名。
+  int sizeofT = sizeof(W); // W的大小。
+
+public:
+  MemoryRiver() {}
+
+  MemoryRiver(const std::string &file_name) : file_name(file_name) {
+    initialise();
+    return;
+  }
+
+  void
+  initialise() { // 如果文件不存在，则会进行初始化操作，建立文件并初始化参数。
+  std::filesystem::path test(file_name);
+    if (std::filesystem::exists(test)) {
+      return;
+    }
+    file.open(file_name, std::fstream::out);
+    int tmp = 0;
+    for (int i = 0; i < info_len; ++i)
+      file.write(reinterpret_cast<char *>(&tmp), sizeof(int));
+    file.close();
+  }
+
+  // 读出第n个int的值赋给tmp，1_base
+  void get_info(int &tmp, int n) {
+    if (n > info_len)
+      return;
+    file.open(file_name);
+    file.seekg((n - 1) * sizeof(int));
+    file.read(reinterpret_cast<char *>(&tmp), sizeof(int));
+    file.close();
+    return;
+  }
+
+  // 将tmp写入第n个int的位置，1_base
+  void write_info(int tmp, int n) {
+    if (n > info_len)
+      return;
+    file.open(file_name);
+    file.seekp((n - 1) * sizeof(int), std::fstream::beg);
+    file.write(reinterpret_cast<char *>(&tmp), sizeof(int));
+    file.close();
+    return;
+  }
+
+  // 在文件合适位置写入类对象t，并返回写入的位置索引index
+  // 位置索引意味着当输入正确的位置索引index，在以下三个函数中都能顺利的找到目标对象进行操作
+  // 位置索引index可以取为对象写入的起始位置
+  void write(W &t, long long place, int size = 1) {
+    file.open(file_name);
+    file.seekp(place);
+    file.write(reinterpret_cast<char *>(&t), sizeofT * size);
+    file.close();
+    return;
+  }
+  void read(W &t, const long long index, int size = 1) {
+    file.open(file_name);
+    file.seekg(index);
+    file.read(reinterpret_cast<char *>(&t), sizeofT * size);
+    file.close();
+    return;
+  }
+};
+int main() {
+  
+}
