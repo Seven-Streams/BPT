ff250ebed5b6d085e6b34c17028f9bb5c7a91584
Yuchuan <Blemiade_Qinchuan@sjtu.edu.cn>
2024-05-17 17:03:47 +0800

    add Cmakelist.

--- BPT.cpp
+++ BPT.cpp
@@ -1670,6 +1670,7 @@ public:
     return;
   }
   ~MemoryRiver() {
+    file.close();
     file.open(file_name + "_config");
     for (int i = 0; i < info_len; i++) {
       file.seekp(i * sizeof(int), std::fstream::beg);
@@ -1734,7 +1735,6 @@ public:
     file.open(file_name);
     file.seekg(place);
     file.read(reinterpret_cast<char *>(&t), sizeofT * size);
-    file.close();
     return;
   }
   void clear() {
@@ -1756,7 +1756,7 @@ inline unsigned long long MyHash(const std::string &txt,
   return ans;
 }
 template <class Value = int, int size = 168, int redundency = 6,
-          int cachesize = 500>
+          int cachesize = 20000>
 class BPT {
 private:
   struct MyData {
